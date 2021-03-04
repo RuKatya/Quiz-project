03262f3aa8f427979900d6383f04e642900e2728
Katya Rukosuev <katyafox93@gmail.com>
2021-03-04 20:22:42 +0200

    1

---
+++ server.js
@@ -0,0 +1,14 @@
+
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+// app.use(express.static(path.join(__dirname+ '/client/build')));
+// app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.use(express.static('client/build'))
+
+const port = process.env.PORT || 4000;
+app.listen(port, ()=>{
+    console.log(`Listening on port ${port}`);
+})
